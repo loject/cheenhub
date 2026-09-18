51e5cece3f0061888bb9ef6b92426629d88b5532
loject <cheengeez13@gmail.com>
2026-09-19 03:41:21 +0700

    ci: publish rolling development release

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -695,16 +695,4 @@ mod tests {
         assert!(updated.contains("[workspace.package]\nversion = \"0.13.0\"\nedition = \"2024\""));
         assert!(updated.contains("[package]\nversion = \"ignored\""));
     }
-
-    #[test]
-    fn pushes_only_the_created_tag_to_origin() {
-        let command = push_git_tag_command("v0.13.0");
-        let args = command
-            .get_args()
-            .map(|arg| arg.to_string_lossy().into_owned())
-            .collect::<Vec<_>>();
-
-        assert_eq!(command.get_program(), "git");
-        assert_eq!(args, ["push", "origin", "refs/tags/v0.13.0"]);
-    }
 }
